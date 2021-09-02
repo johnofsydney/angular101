37902d4ba9e765d2e0d8aa6c38d334fa822b786e
John Coote <john.coote@mable.com.au>
2021-09-02 20:11:07 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+angular
+  .module('flapperNews', [])
+  .controller(
+    'MainCtrl',
+    [
+      '$scope',
+      function($scope){
+        $scope.test = 'Hello World!'
+        $scope.teste = 'Hello Fellow!'
+        $scope.posts = [
+          {title: 'post 1', upvotes: 5},
+          {title: 'post 2', upvotes: 2},
+          {title: 'post 3', upvotes: 15},
+          {title: 'post 4', upvotes: 9},
+          {title: 'post 5', upvotes: 4},
+        ]
+        $scope.addPost = function() {
+          if (!$scope.title || $scope.title === '') {
+            console.log('empty')
+            return
+          }
+          $scope.posts.push(
+            {title: $scope.title, upvotes: 0}
+          )
+          $scope.title = ''
+        }
+        $scope.incrementUpvotes = function (post) {
+          post.upvotes += 1;
+        }
+      }
+    ]
+  )
