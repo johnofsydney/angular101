8e44bb3c071528353d2c7491ed28e1604265a803
John Coote <john.coote@mable.com.au>
2021-09-03 11:34:23 +1000

    added bootstrap

--- app.js
+++ app.js
@@ -16,13 +16,18 @@ angular
         ]
         $scope.addPost = function() {
           if (!$scope.title || $scope.title === '') {
-            console.log('empty')
+            console.log('title was empty')
             return
           }
           $scope.posts.push(
-            {title: $scope.title, upvotes: 0}
+            {
+              title: $scope.title,
+              link: $scope.link,
+              upvotes: 0
+            }
           )
           $scope.title = ''
+          $scope.link = ''
         }
         $scope.incrementUpvotes = function (post) {
           post.upvotes += 1;
